0935ee39654567a0ac60aab65608359fda37b5fb
amanikaca <amanikaca@gmail.com>
2025-06-06 20:01:03 +0530

    operators.py

--- operators.py
+++ operators.py
@@ -1,6 +1,6 @@
 print("enter two numbers")
-a=int(input("enter the first number : "))
-b=int(input("enter the second number : "))
+a=int(input("enter the first number (a) : "))
+b=int(input("enter the second number(b) : "))
 
 mylist = list(map(int, input("Enter the numbers separated by space: ").split()))
 
@@ -58,3 +58,60 @@ print("\n    ----------membership operators ----------")
 print(mylist)
 print(f"a in mylist :{a in mylist}")
 print(f"b not in mylist :{a not  in mylist}")
+
+
+#output
+'''
+C:\Users\Dell\Desktop\-\Python>py operators.py
+enter two numbers
+enter the first number (a) : 3
+enter the second number (b): 5
+Enter the numbers separated by space: 4 6 3 7
+
+ ----------Arithemetic operators ----------
+add : 8
+subtraction :-2
+multiplication : 15
+division : 0.6
+floor division : 0
+exponential : 243
+modulus : 3
+
+ ----------Assignment operators ----------
+x+=b: 8
+x-=b: 3
+x*=b: 15
+x/=b: 3.0
+
+ ----------comparison  operators ----------
+a==b :False
+a!=b :True
+a<=b :True
+a>=b :False
+a<b :True
+a>b :False
+
+ ----------logical  operators ----------
+a<5 and b>5 :False
+a<5 or b>5 :True
+not(a>5) :True
+
+ ----------bitwise operators ----------
+a&b :1
+a|b :7
+a^b :6
+a<<b :96
+a>>b :0
+~a :-4
+
+ ----------special operators ----------
+
+    ----------identity operators ----------
+a is b :False
+a is not b :True
+
+    ----------membership operators ----------
+[4, 6, 3, 7]
+a in mylist :True
+b not in mylist :False
+'''
