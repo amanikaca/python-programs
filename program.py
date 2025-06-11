70ac99ff25e8c2f8109942702a37e3ab6f26b493
amanikaca <amanikaca@gmail.com>
2025-06-11 18:30:27 +0530

    program.py

--- program.py
+++ program.py
@@ -3,3 +3,9 @@ b=int(input("enter 2nd number : "))
 print(f"XOR ={a^b}")
 print(f"right shift ={a<<b}")
 print(f"left shift ={a>>b}")
+#output
+#enter 1st number : 4
+#enter 2nd number : 5
+#XOR =1
+#right shift =128
+#left shift =0
