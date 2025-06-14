d7dba9a1e75d0b379e768cadfec3caafd18f90d1
amanikaca <amanikaca@gmail.com>
2025-06-14 20:01:31 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,12 @@
+# Simple Python program 
+
+text = input("Enter a string: ")
+
+reversed_text = text[::-1]
+
+
+print("Reversed string:", reversed_text)
+
+#output
+#Enter a string: hello
+#Reversed string: olleh
